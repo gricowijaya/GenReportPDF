20e86b8bf81dd5c714c7650784f8ccf5f1889331
Gede Rico Wijaya <gricowijaya@gmail.com>
2021-06-13 13:57:47 +0800

    Menambah Script Databse

--- GeneratedReportPDF/src/main/java/script_database/script_pembuatan_tabel.sql
+++ GeneratedReportPDF/src/main/java/script_database/script_pembuatan_tabel.sql
@@ -1,43 +1,70 @@
-
-CREATE TABLE tb_barang ( 
-    id_barang INT PRIMARY KEY NOT NULL, 
-    in_stok INT DEFAULT 0, 
-    out_stok INT DEFAULT 0, 
-    sisa_stok INT DEFAULT 0, 
-    tanggal TIMESTAMP
+-- berhubungan dengan pegawai, stok, histori_pendataan
+CREATE TABLE tb_barang (
+    id_barang INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+    nama_barang VARCHAR(20) NOT NULL,
+    kode_satuan INT,
+    kode_kategori INT,
+    id_pegawai VARCHAR(30), 
+    status VARCHAR(15) DEFAULT "ACTIVE",
+    FOREIGN KEY (id_pegawai) REFERENCES tb_pegawai(id_pegawai),
+    FOREIGN KEY (kode_satuan) REFERENCES tb_satuan(kode_satuan),
+    FOREIGN KEY (kode_kategori) REFERENCES tb_kategori(kode_kategori)
 );
 
 CREATE TABLE tb_pegawai ( 
-    id_pegawai VARCHAR(12) PRIMARY KEY NOT NULL,  
+    id_pegawai VARCHAR(30) PRIMARY KEY NOT NULL,  
+    password VARCHAR(20),
     nama_depan VARCHAR(12) NOT NULL,
     nama_belakang VARCHAR(12) NOT NULL, 
-    jabatan VARCHAR(12), 
-    nomor_telp VARCHAR(13)  
+    jabatan INT, 
+    nomor_telp VARCHAR(13),  
+    status VARCHAR(15) DEFAULT "ACTIVE"
 );
 
-CREATE TABLE tb_histori_pegawai ( 
-    id_history INT PRIMARY KEY NOT NULL, 
-    id_barang INT, 
-    in_stok INT, 
-    out_stok INT, 
+-- CREATE TABLE tb_jabatan ( 
+--     id_jabatan INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+--     jabatan VARCHAR(30)
+--     status VARCHAR(15) DEFAULT "ACTIVE"
+-- );
+
+CREATE TABLE tb_stok ( 
+    id_data_stok INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id_barang INT,
+    stok_awal INT DEFAULT 0,
+    in_stok INT DEFAULT 0, 
+    out_stok INT DEFAULT 0, 
+    sisa_stok INT DEFAULT 0, 
+    stok_gudang INT DEFAULT 0,
     tanggal TIMESTAMP,
     FOREIGN KEY (id_barang) REFERENCES tb_barang(id_barang)
 );
 
-CREATE TABLE tb_satuan(
-    kode_satuan VARCHAR(12) PRIMARY KEY NOT NULL, 
-    satuan VARCHAR(12)
+ALTER TABLE tb_barang ADD COLUMN stok_display INT DEFAULT 0 ;
+ALTER TABLE tb_barang ADD COLUMN stok_masuk_display INT DEFAULT 0; 
+ALTER TABLE tb_barang ADD COLUMN stok_gudang INT DEFAULT 0 AFTER stok_masuk_display; 
+ALTER TABLE tb_barang ADD COLUMN terjual INT DEFAULT 0; 
+
+
+CREATE TABLE tb_histori_pendataan( 
+    id_histori INT PRIMARY KEY NOT NULL AUTO_INCREMENT,  
+    id_barang INT, 
+    stok_display INT DEFAULT 0,
+    stok_masuk_display INT DEFAULT 0, 
+    stok_gudang INT DEFAULT 0,
+    stok_masuk_gudang INT DEFAULT 0,
+    tanggal TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (id_barang) REFERENCES tb_barang(id_barang),
 );
 
-ALTER TABLE tb_barang ADD COLUMN kode_satuan VARCHAR(12);
-ALTER TABLE tb_barang ADD FOREIGN KEY (kode_satuan) REFERENCES tb_satuan(kode_satuan);
+CREATE TABLE tb_satuan(
+    kode_satuan INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+    satuan VARCHAR(12),
+    status VARCHAR(15) DEFAULT "ACTIVE"
+);
+
 
 CREATE TABLE tb_kategori_barang (
-    kode_kategori VARCHAR(12) PRIMARY KEY NOT NULL,
+    kode_kategori INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     kategori VARCHAR(12),
+    status VARCHAR(15) DEFAULT "ACTIVE"
 );
-
-ALTER TABLE tb_barang ADD COLUMN kode_kategori VARCHAR(12) ;
-
-ALTER TABLE tb_barang ADD FOREIGN KEY (kode_kategori) 
-REFERENCES tb_kategori_barang(kode_kategori);
--- GeneratedReportPDF/src/main/java/script_database/script_pembuatan_trigger.sql
+++ GeneratedReportPDF/src/main/java/script_database/script_pembuatan_trigger.sql
@@ -1,11 +1,14 @@
+-- membuat trigger penambahan_histori barang agar otomatis terupdate 
+DROP TRIGGER IF EXISTS penambahan_histori;
 DELIMITER $$
 
-CREATE TRIGGER penambahan_history BEFORE UPDATE 
-on tb_histori_pendataan FOR EACH ROW
+CREATE TRIGGER penambahan_histori BEFORE UPDATE 
+on tb_barang FOR EACH ROW
 BEGIN
-    INSERT INTO tb_histori_pendataan (id_barang, in_stok, out_stok, sisa_stok, tanggal)
-    VALUES (old.id_barang, old.in_stok, old.out_stok, old.sisa_stok, old.tanggal);
+    INSERT INTO tb_histori_pendataan VALUES
+
 END $$
 
 DELIMITER ; 
- 
+
+UPDATE tb_barang SET stok_masuk_display = 4, SET stok_gudang = ((SELECT stok_gudang FROM tb_barang WHERE nama_barang = Malboro) - 4) WHERE nama_barang = Malboro 
